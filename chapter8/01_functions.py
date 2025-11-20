4473b5578f56002cb9de5fdf6481c4f07e6e795f
parthasarathisantra <parthasarathisantra227@gmail.com>
2025-11-20 11:02:11 +0530

    chapter 8

---
+++ chapter8/01_functions.py
@@ -0,0 +1,17 @@
+# function defination
+def avg():
+    a = int(input("Enter your number: "))
+    b = int(input("Enter your number: "))
+    c = int(input("Enter your number: "))
+
+    average = (a+b+c)/3
+    print(average)
+
+avg()   # function call
+print("Thank you")
+avg()
+print("Thank you")
+avg()
+print("Thank you")
+avg()
+avg()
